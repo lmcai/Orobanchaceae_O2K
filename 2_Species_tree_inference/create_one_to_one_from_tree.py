6a6182178a32c090585378e326931ee442fb39cd
Liming Cai <34404264+lmcai@users.noreply.github.com>
2024-04-22 22:19:20 -0500

    Create create_one_to_one_from_tree.py

---
+++ 2_Species_tree_inference/create_one_to_one_from_tree.py
@@ -0,0 +1,40 @@
+from ete3 import Tree
+import os
+
+files=os.listdir('.')
+files=[i for i in files if i.endswith('.treefile')]
+
+for f in files:
+	t=Tree(f)
+	#reroot
+	amb=[node.name for node in t if node.name.startswith('Amb')]
+	amb_node=t&amb[0]
+	t.root(amb_root)
+	to_keep=[]
+	shortest={}
+	for node in t:
+		sp=node.name.split('_')[0]
+		if not sp in to_keep:
+			to_keep.append(node.name)
+			shortest[sp]=node.get_distance(amb_node)
+		else:
+			if node.get_distance(amb_node)<shortest[sp]:
+				previous=[i for i in to_keep if i.startswith(sp)]
+				to_keep.remove(previous)
+				to_keep.append(node.name)
+				shortest[sp]=node.get_distance(amb_node)
+			elif node.get_distance(amb_node)==shortest[sp]:
+				if not node.name.endswith('_2'):
+					previous=[i for i in to_keep if i.startswith(sp)]
+					if len(node.name) >len(previous):
+						to_keep.remove(previous)
+						to_keep.append(node.name)
+						shortest[sp]=node.get_distance(amb_node)
+	recs=SeqIO.index('')
+	out=open()
+	for i in to_keep:
+		try:
+			d=SeqIO.write(recs[i],out,'fasta')
+		except KeyError:
+			print(i)
+	out.close()
