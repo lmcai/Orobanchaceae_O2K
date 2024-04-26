915620820d948ca5b8f1373ad35aaa8c4885dcc9
Liming Cai <34404264+lmcai@users.noreply.github.com>
2024-04-25 21:18:17 -0500

    Update create_one_to_one_from_tree.py

--- 2_Species_tree_inference/create_one_to_one_from_tree.py
+++ 2_Species_tree_inference/create_one_to_one_from_tree.py
@@ -8,8 +8,14 @@ for f in files:
 	t=Tree(f)
 	#reroot
 	amb=[node.name for node in t if node.name.startswith('Amb')]
-	amb_node=t&amb[0]
-	t.root(amb_root)
+	if len(amb):
+		amb_node=t&amb[0]
+		t.set_outgroup(amb_node)
+	else:
+		cin=[node.name for node in t if node.name.startswith('Cin')]
+		if len(cin):
+			cin_node=t&cin[0]
+			t.set_outgroup(cin_root)
 	to_keep=[]
 	shortest={}
 	for node in t:
@@ -20,21 +26,22 @@ for f in files:
 		else:
 			if node.get_distance(amb_node)<shortest[sp]:
 				previous=[i for i in to_keep if i.startswith(sp)]
-				to_keep.remove(previous)
+				to_keep.remove(previous[0])
 				to_keep.append(node.name)
 				shortest[sp]=node.get_distance(amb_node)
 			elif node.get_distance(amb_node)==shortest[sp]:
 				if not node.name.endswith('_2'):
 					previous=[i for i in to_keep if i.startswith(sp)]
 					if len(node.name) >len(previous):
-						to_keep.remove(previous)
+						to_keep.remove(previous[0])
 						to_keep.append(node.name)
 						shortest[sp]=node.get_distance(amb_node)
-	recs=SeqIO.index('')
-	out=open()
+	recs=SeqIO.index(f.split('.')[0]+'.filter.fas','fasta')
+	out=open(f.split('.')[0]+'.one2one.fas','w')
 	for i in to_keep:
 		try:
 			d=SeqIO.write(recs[i],out,'fasta')
 		except KeyError:
 			print(i)
+			pass
 	out.close()
