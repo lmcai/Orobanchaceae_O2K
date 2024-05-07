dfa7456f10eb0aed8164516b714c5b766810fb5c
Liming Cai <34404264+lmcai@users.noreply.github.com>
2024-05-07 17:43:24 -0500

    Update create_one_to_one_from_tree.py

--- 2_Species_tree_inference/create_one_to_one_from_tree.py
+++ 2_Species_tree_inference/create_one_to_one_from_tree.py
@@ -10,13 +10,13 @@ for f in files:
 	#reroot
 	amb=[node.name for node in t if node.name.startswith('Amb')]
 	if len(amb):
-		amb_node=t&amb[0]
-		t.set_outgroup(amb_node)
+		root_node=t&amb[0]
+		t.set_outgroup(root_node)
 	else:
 		cin=[node.name for node in t if node.name.startswith('Cin')]
 		if len(cin):
-			cin_node=t&cin[0]
-			t.set_outgroup(cin_root)
+			root_node=t&cin[0]
+			t.set_outgroup(root_node)
 	to_keep=[]
 	shortest={}
 	sp_pool=[]
@@ -24,21 +24,21 @@ for f in files:
 		sp=node.name.split('_')[0]
 		if not sp in sp_pool:
 			to_keep.append(node.name)
-			shortest[sp]=node.get_distance(amb_node)
+			shortest[sp]=node.get_distance(root_node)
 			sp_pool.append(sp)
 		else:
-			if node.get_distance(amb_node)<shortest[sp]:
+			if node.get_distance(root_node)<shortest[sp]:
 				previous=[i for i in to_keep if i.startswith(sp)]
 				to_keep.remove(previous[0])
 				to_keep.append(node.name)
-				shortest[sp]=node.get_distance(amb_node)
-			elif node.get_distance(amb_node)==shortest[sp]:
+				shortest[sp]=node.get_distance(root_node)
+			elif node.get_distance(root_node)==shortest[sp]:
 				if not node.name.endswith('_2'):
 					previous=[i for i in to_keep if i.startswith(sp)]
 					if len(node.name) >len(previous):
 						to_keep.remove(previous[0])
 						to_keep.append(node.name)
-						shortest[sp]=node.get_distance(amb_node)
+						shortest[sp]=node.get_distance(root_node)
 	recs=SeqIO.index(f.split('.')[0]+'.filter.fas','fasta')
 	out=open(f.split('.')[0]+'.one2one.fas','w')
 	for i in to_keep:
